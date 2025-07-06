ee54476c0533b323d39283be211d35b05442fea8
yura <yurii@niuitmo.ru>
2025-07-06 18:56:35 +0300

    fix icons

--- public/script.js
+++ public/script.js
@@ -6,15 +6,15 @@ let expanded = false;
 let toggled = false;
 
 button.onclick = () => {
-    toggled = true;
-    expanded = !expanded;
+  toggled = true;
+  expanded = !expanded;
 
-    button.setAttribute("aria-expanded", expanded.toString());
-    span.innerHTML = expanded ? 'Закрыть меню' : 'Открыть меню'
-    if (expanded) {
-        ul.classList.add("header__links_opened", "header__links-toggled")
-    } else {
-        ul.classList.remove("header__links_opened")
-        ul.classList.add("header__links-toggled")
-    }
+  button.setAttribute("aria-expanded", expanded.toString());
+  span.innerHTML = expanded ? 'Закрыть меню' : 'Открыть меню'
+  if (expanded) {
+    ul.classList.add("header__links_opened", "header__links-toggled")
+  } else {
+    ul.classList.remove("header__links_opened")
+    ul.classList.add("header__links-toggled")
+  }
 }
